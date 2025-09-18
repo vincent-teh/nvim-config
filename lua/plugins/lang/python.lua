1ee199c1069f2a4aab4336ab56502985892c34a5
Yu Sheng Teh <yu.sheng.teh@ericsson.com>
2025-09-18 15:39:27 +0200

    fix telescope versioning issue

--- lua/plugins/lang/python.lua
+++ lua/plugins/lang/python.lua
@@ -14,8 +14,7 @@ vim.api.nvim_create_autocmd("FileType", {
 return {
 	"linux-cultist/venv-selector.nvim",
 	dependencies = {
-		"neovim/nvim-lspconfig",
-		{ "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+		{ "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
 	},
 	ft = "python",
 	opts = {
