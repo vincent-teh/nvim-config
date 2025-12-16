3ed706b01f060efc924f8581c29e1a873cd171f0
Yu Sheng Teh <yu.sheng.teh@ericsson.com>
2025-12-16 11:02:56 +0100

    disable markdown autoscroll

--- lua/plugins/lang/markdown.lua
+++ lua/plugins/lang/markdown.lua
@@ -22,5 +22,22 @@ return {
 		build = function()
 			vim.fn["mkdp#util#install"]()
 		end,
+		init = function()
+			vim.g.mkdp_auto_close = 0
+			vim.g.mkdp_refresh_slow = 0
+			vim.g.mkdp_command_for_global = 0
+			vim.g.mkdp_open_to_the_world = 0
+			vim.g.mkdp_open_ip = ""
+			vim.g.mkdp_echo_preview_url = 0
+			vim.g.mkdp_browserfunc = ""
+			vim.g.mkdp_preview_options = {
+				disable_sync_scroll = 1,
+			}
+			vim.g.mkdp_markdown_css = ""
+			vim.g.mkdp_highlight_css = ""
+			vim.g.mkdp_port = ""
+			vim.g.mkdp_page_title = "「${name}」"
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
 	},
 }
